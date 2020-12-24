ef9feab94477f30deaed5c0eccc806344f0ec0c5
rd9911 <azizaziz9911@mail.ru>
2020-12-24 22:34:50 +0900

    Updated VS code configs

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -12,7 +12,7 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual envipythonronment?"
         ) from exc
     execute_from_command_line(sys.argv)
 
