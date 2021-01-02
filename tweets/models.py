859e7cb33b45cb9f15d223f21d4869cbd31dbc9d
rd9911 <azizaziz9911@mail.ru>
2021-01-02 19:58:33 +0900

    Added 'like' model

--- tweets/models.py
+++ tweets/models.py
@@ -6,12 +6,19 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey('Tweet', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+
 
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) # to add feature that will inform retweets that tweet was deleted: null=True, SET_NULL
+    like = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
-    
+    timestamp = models.DateTimeField(auto_now=True)
+
     # def __str__(self):
     #     return self.content
 
