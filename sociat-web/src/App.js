b2d54a25c69f10eedfb4bb48a7477c7f06439ee1
rd9911 <azizaziz9911@mail.ru>
2021-02-04 23:15:19 +0900

    Action button improve

--- sociat-web/src/App.js
+++ sociat-web/src/App.js
@@ -1,63 +1,15 @@
-import React, {useEffect, useState} from 'react';
-
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function loadTweets(callback) {
-  const xhr = new XMLHttpRequest()
-  const method = 'GET'
-  const url = 'http://localhost:8000/api/tweet'
-  const responseType = 'json'
-  xhr.responseType = responseType
-  xhr.open(method, url)
-
-  xhr.onload = function() {
-      callback(xhr.response, xhr.status)
-  }
-  xhr.onerror = function (e) {
-    console.log(e)
-    callback({'message': 'The request was an error'}, 400)
-  }
-  xhr.send()
-}
+import {TweetList} from './tweets'
 
 
-function ActionBtn(props) {
-  const {tweet, action} = props
-  const className = props.className ? props.className : 'btn btn-primary btn-sm'
-  return action.type === 'like' ? <button className={className}> {tweet.likes} Likes</button> : null
-}
 
 
-function Tweet(props) {
-  const {tweet} = props
-  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
-  return <div className={className}>
-    <p>
-      {tweet.id} - {tweet.content}
-    </p>
-    <div className='btn btn-group'>
-      <ActionBtn tweet={tweet} action={{type: 'like'}}/>
-      <ActionBtn tweet={tweet} action={{type: 'unlike'}}/>
-      <ActionBtn tweet={tweet} action={{type: 'retweet'}}/>
-    </div>
-  </div>
-}
 
-
-function App() {
-  const [tweets, setTweets] = useState([])
-
-  useEffect(() => {
-    const myCallback = (response, status) => {
-      if (status === 200) {
-        setTweets(response)
-      } else {
-        alert('There was an error.')
-      }
-    }
-    loadTweets(myCallback)
-  }, [])
+export function App() {
+  
 
   return (
     <div className="App">
@@ -67,9 +19,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-            {tweets.map((item, index) => {
-              return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index} - ${item}`}/>
-            })}
+            <TweetList />
         </div>
         <a
           className="App-link"
