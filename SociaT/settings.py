5222579a6b59ff703eb676fc8fca761250341ba0
rd9911 <azizaziz9911@mail.ru>
2021-02-05 23:21:40 +0900

    Render React app by Django

--- SociaT/settings.py
+++ SociaT/settings.py
@@ -142,7 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-root')
 
 CORS_ALLOW_ALL_ORIGINS = True # any website has an access to my api
 CORS_URLS_REGEX = r'^/api/.*$'
